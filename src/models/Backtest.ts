b1d9bc8781506a8db0bf85801d571f4580941481
Dmitry Fokin <fokind1@yandex.ru>
2019-11-05 21:32:53 +0300

    добавлен execute

--- src/models/Backtest.ts
+++ src/models/Backtest.ts
@@ -1,5 +1,9 @@
+import { streamTrades } from "backtesting";
 import { ObjectID } from "mongodb";
-import { Edm } from "odata-v4-server";
+import { Edm, odata } from "odata-v4-server";
+import connect from "../connect";
+
+const collectionName = "backtest";
 
 export class Backtest {
   @Edm.Key
@@ -43,5 +47,57 @@ export class Backtest {
 
   constructor(data: any) {
     Object.assign(this, data);
+  }
+
+  @Edm.Action
+  public async execute(@odata.result result: any): Promise<number> {
+    const { _id: key } = result;
+    // tslint:disable-next-line: variable-name
+    const _id = new ObjectID(key);
+    const db = await connect();
+    const backtest = new Backtest(
+      await db.collection(collectionName).findOne({ _id })
+    );
+    const {
+      asset,
+      currency,
+      exchange,
+      period,
+      begin,
+      end,
+      indicators,
+      initialBalance,
+      code
+    } = backtest;
+
+    const rs = streamTrades({
+      exchange,
+      currency,
+      asset,
+      period,
+      start: begin,
+      end,
+      indicators: JSON.parse(indicators),
+      code,
+      initialBalance
+    });
+
+    const trades: Array<{ amount: number }> = [];
+    rs.on("data", chunk => trades.push(JSON.parse(chunk)));
+
+    return new Promise(resolve => {
+      rs.on("end", resolve);
+    })
+      .then(() =>
+        db.collection(collectionName).updateOne(
+          { _id },
+          {
+            $set: {
+              finalBalance: Math.abs(trades[trades.length - 1].amount)
+            }
+          }
+        )
+      )
+      .then(r => r.modifiedCount);
   }
 }
